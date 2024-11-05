e9cd91733606cde468cd680419ada2f86c4d67b7
David <rodriguezj.david@javeriana.edu.co>
2024-11-05 14:36:22 -0500

    New interfaces

--- lib/ui/create_assignment/create_assignment_model.dart
+++ lib/ui/create_assignment/create_assignment_model.dart
@@ -1,7 +1,15 @@
+import '/flutter_flow/flutter_flow_drop_down.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
 import 'create_assignment_widget.dart' show CreateAssignmentWidget;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class CreateAssignmentModel extends FlutterFlowModel<CreateAssignmentWidget> {
   ///  State fields for stateful widgets in this page.
@@ -11,7 +19,7 @@ class CreateAssignmentModel extends FlutterFlowModel<CreateAssignmentWidget> {
   FocusNode? assignmentNameFocusNode;
   TextEditingController? assignmentNameTextController;
   String? Function(BuildContext, String?)?
-      assignmentNameTextControllerValidator;
+  assignmentNameTextControllerValidator;
   String? _assignmentNameTextControllerValidator(
       BuildContext context, String? val) {
     if (val == null || val.isEmpty) {
@@ -27,7 +35,7 @@ class CreateAssignmentModel extends FlutterFlowModel<CreateAssignmentWidget> {
   FocusNode? assignmentDescriptionFocusNode;
   TextEditingController? assignmentDescriptionTextController;
   String? Function(BuildContext, String?)?
-      assignmentDescriptionTextControllerValidator;
+  assignmentDescriptionTextControllerValidator;
   // State field(s) for AssignmentDificulty widget.
   double? assignmentDificultyValue;
   // State field(s) for AssignmentType widget.
